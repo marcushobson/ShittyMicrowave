c6c82a1dff7f39338559335308392a55a91f49e2
marcushobson <marcus@hmbc.co.uk>
2018-04-28 01:00:15 +0100

    Developed the world's worst microwave.

---
+++ Microwave/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Media;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Microwave
+{
+	class ShittyMicrowave
+	{
+		[DllImport("winmm.dll", EntryPoint = "mciSendString")]
+		public static extern int mciSendStringA(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+		static void Main(string[] args)
+		{
+			new ShittyMicrowave(true);
+		}
+
+		public ShittyMicrowave(bool closesAfterDing)
+		{
+			Open();
+			Ding();
+			Serve();
+			Close();
+		}
+
+		private void Open()
+		{
+			mciSendStringA("set CDAudio door open", "", 127, 0);
+		}
+
+		private void Ding()
+		{
+			new SoundPlayer(Assembly.GetExecutingAssembly().GetManifestResourceStream("Microwave.ding.wav")).Play();
+		}
+
+		private void Serve()
+		{
+			Thread.Sleep(5000);
+		}
+
+		private void Close()
+		{
+			mciSendStringA("set CDAudio door closed", "", 127, 0);
+		}
+	}
+}
